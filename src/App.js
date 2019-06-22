50e41d548dc074ef8da119606ec30c74f0fd5287
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-22 15:33:27 +0200

    enter

--- src/App.js
+++ src/App.js
@@ -1,67 +1,23 @@
 import React, { Component } from 'react';
-import { random } from 'lodash';
-import 'typeface-roboto';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
-import QuoteMachine from './components/QuoteMachine';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
 
-const styles = {
-  container: {
-    alignItems: 'center',
-    display: 'flex',
-    height: '100vh' 
-  }
-}
+let marked = require("marked");
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quotes: [],
-      selectedQuoteIndex: null
-    }
-    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
-    this.selectQuoteIndex = this.generateNewQuoteIndex.bind(this);
-  }
-  
-  componentDidMount() {
-    fetch('https://gist.githubusercontent.com/shreyasminocha/7d5dedafc1fe158f82563c1223855177/raw/325d51aca7165b2498971afcff9bed286a52dc0e/quotes.json')
-      .then(data => data.json())
-      .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
-  }
-  
-  get selectedQuote() {
-    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
-      return undefined;
-    }
-    return this.state.quotes[this.state.selectedQuoteIndex];
-  }
-  
-  /**
-   * Returns an integer representing an index in array of quotes (state.quotes) 
-   */
-   generateNewQuoteIndex() {
-    if (!this.state.quotes.length) {
-      return undefined;
-    }
-    return random(0, this.state.quotes.length - 1);  
-  }
-
-  assignNewQuoteIndex() {
-    this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() });
-  }
-
   render() {
     return (
-      <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
-        <Grid xs={11} lg={8} item>
-          {
-            this.selectedQuote ? <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} /> : null
-          }
-        </Grid>
-      </Grid>
+      <div className="App">
+        <div>
+          <FormGroup controlId="formControlsTextarea">
+            <ControlLabel>Markdown Input</ControlLabel>
+            <FormControl componentClass="textarea" placeholder="Enter< Markdown"></FormControl>
+          </FormGroup>
+        </div>
+      </div>
     );
   }
 }
 
-export default withStyles(styles)(App);
+export default App;
