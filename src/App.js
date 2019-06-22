0896ebb56faf83fff5f5a9840ec8115782635854
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-22 19:33:29 +0200

    enter

--- src/App.js
+++ src/App.js
@@ -5,14 +5,34 @@ import Form from 'react-bootstrap/Form'
 let marked = require("marked");
 
 class App extends Component {
+
+  state = {
+    markdown: ""
+  }
+
+  updateMarkdown = function (markdown) {
+    this.setState({ markdown });
+  }
+
   render() {
+    let { markdown } = this.state;
+    console.log(markdown);
     return (
       <div className="App container">
         <div>
+          <textarea id="editor"></textarea>
+        </div>
+        <div>
           <Form.Group controlId="formControlsTextarea">
             <Form.Label>Markdown Input</Form.Label>
-            <Form.Control componentClass="textarea" placeholder="Enter Markdown"></Form.Control>
+            <Form.Control componentClass="textarea" placeholder="Enter Markdown" value={markdown} onChange={(event) => this.updateMarkdown(event.target.value)}></Form.Control>
           </Form.Group>
+        </div>
+        <div>
+          <h3>Markdown Output</h3>
+          <div dangerouslySetInnerHTML={{ __html: marked(markdown) }}>
+
+          </div>
         </div>
       </div>
     );
