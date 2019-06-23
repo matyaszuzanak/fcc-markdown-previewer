5aa4233829f493a28c6c02e21b594949f86d92e2
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-23 17:11:02 +0200

    enter

--- src/App.js
+++ src/App.js
@@ -2,40 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from 'react-bootstrap/Form'
 
-let marked = require("marked");
-
 class App extends Component {
-
-  state = {
-    markdown: ""
-  }
-
-  updateMarkdown = function (markdown) {
-    this.setState({ markdown });
-  }
-
   render() {
-    let { markdown } = this.state;
-    console.log(markdown);
     return (
-      <div className="App container">
-        <div>
-          <textarea id="editor"></textarea>
-        </div>
-        <div>
-          <Form.Group controlId="formControlsTextarea">
-            <Form.Label>Markdown Input</Form.Label>
-            <Form.Control componentClass="textarea" placeholder="Enter Markdown" value={markdown} onChange={(event) => this.updateMarkdown(event.target.value)}></Form.Control>
-          </Form.Group>
-        </div>
-        <div>
-          <h3>Markdown Output</h3>
-          <div dangerouslySetInnerHTML={{ __html: marked(markdown) }}>
-
+      <div>
+        <h1>Markdown Previewer</h1>
+        <div className="container">
+          <div className="left">
+            <textarea id="editor" />
+          </div>
+          <div className="right">
+            <div id="preview" />
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
