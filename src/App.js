8b91e7dbc780af5269a14eb49d9109a7e2c05417
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-24 11:24:46 +0200

    enter

--- src/App.js
+++ src/App.js
@@ -8,6 +8,48 @@ const initialMarkdown = `
 # Header 1
 ## Header 2
 ### Header 3
+#### Header 4
+##### Header 5
+
+### List
+
+- list item one
+- list item two
+- list item three
+
+### Links
+
+[FreeCodeCamp](https://learn.freecodecamp.org)
+
+[Google](https://www.google.com "World's most popular search engine")
+
+### Text Decorations
+
+*italic*
+
+**bold**
+
+***italic and bold***
+
+### Image
+
+![React logo](https://logos-download.com/wp-content/uploads/2016/09/React_logo_small.png "React logo")
+
+### Blockquote
+
+> "Whether you think you can or you can't, either way you are right." --Henry Ford
+
+## Code
+
+\`npm install create-react-app\`
+
+### Function
+
+\`\`\`
+function multiplyByTwo(a) {
+  return a *= 2 
+}
+\`\`\`
 `
 
 class App extends Component {
@@ -19,13 +61,15 @@ class App extends Component {
     }
   }
 
+  handleChange = e => this.setState({ markdown: e.target.value })
+
   render() {
     return (
       <div>
         <h1 id="top-header">Markdown Previewer</h1>
         <div className="box">
           <div className="left">
-            <textarea id="editor" />
+            <textarea id="editor" value={this.state.markdown} onChange={this.handleChange}/>
           </div>
           <div className="right">
             <div id="preview" dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}} />
